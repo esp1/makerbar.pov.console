74bb88e5ea2b0937329fe8504b4765250bd2756d
Edwin Park <esp@mecha1.com>
2014-03-29 17:03:15 -0400

    use hiphip to speed up(x1000!) array ops

--- project.clj
+++ project.clj
@@ -3,5 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[quil "2.0.0-SNAPSHOT"]
-                 [org.clojure/clojure "1.6.0"]])
+  :profiles {:dev {:dependencies [[criterium "0.4.3"]]}}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [prismatic/hiphip "0.2.0"]
+                 [quil "2.0.0-SNAPSHOT"]])
--- src/makerbar/pov/console/draw.clj
+++ src/makerbar/pov/console/draw.clj
@@ -1,30 +1,31 @@
 (ns makerbar.pov.console.draw
   (:import [processing.core PImage])
-  (:require [makerbar.pov.console.state :as s]
+  (:require [hiphip.int :as int]
+            [makerbar.pov.console.state :as s]
             [makerbar.pov.console.util :refer (draw)]
             [quil.core :as q]))
 
 
-; Drawing fns
+(defn bounded [x] (min (max 0 x) 255))
 
-(defn adjust-color [])
+(defn adjust-color-channel
+  "Apply brightness (+) and contrast (*) to color channel value"
+  [value brightness contrast]
+  (bounded (int (+ (* value contrast) brightness))))
 
 (defn adjust-color
-  "Apply contrast (*) and brightness (+) to color"
-  [c contrast brightness]
-  (letfn [(bounded [x] (min (max 0 x) 255))
-          (adjust [x] (bounded (int (+ (* x contrast) brightness))))]
-    (let [r (adjust (bit-and (bit-shift-right c 16) 0xff))
-          g (adjust (bit-and (bit-shift-right c 8) 0xff))
-          b (adjust (bit-and c 0xff))]
-      (bit-or 0xff000000 (bit-shift-left r 16) (bit-shift-left g 8) b))))
+  "Apply brightness (+) and contrast (*) to rgb color"
+  [c brightness contrast]
+  (let [r (adjust-color-channel (bit-and (bit-shift-right c 16) 0xff) brightness contrast)
+        g (adjust-color-channel (bit-and (bit-shift-right c 8) 0xff) brightness contrast)
+        b (adjust-color-channel (bit-and c 0xff) brightness contrast)]
+    (bit-or 0xff000000 (bit-shift-left r 16) (bit-shift-left g 8) b)))
 
 (defn draw-image []
   (draw @s/graphics
         (.background @s/graphics 0)
         
         (when-let [img (:image @s/state)]
-          (println "got" img)
           (let [img-width (.width img)
                 img-height (.height img)
                 pimg (PImage. img-width img-height)
@@ -32,27 +33,21 @@
                         img-x-offset img-y-offset img-scale
                         pov-x-offset pov-y-offset]} @s/state]
             ; adjust contrast and brightness
-            (println "load pixels")
             (.loadPixels img)
-            (println "adjusting contrast and brightness")
             (let [img-pixels (.pixels img)
                   pimg-pixels (.pixels pimg)]
-              (println "range:" (* img-width img-height))
               (doseq [i (range 0 (* img-width img-height))]
-                (let [color (aget img-pixels i)
-                      adjusted-color (adjust-color color contrast brightness)]
-                  (aset pimg-pixels i adjusted-color)))
-              (println "end range")
+                (let [color (int/aget img-pixels i)
+                      adjusted-color (adjust-color color brightness contrast)]
+                  (int/aset pimg-pixels i adjusted-color)))
               (.updatePixels pimg))
             
             ; flip image
             (when (:flip-image @s/state)
-              (println "flip image")
               (.scale @s/graphics -1 1)
               (.translate @s/graphics (- (- s/pov-width 1)) 0))
             
             ; draw image
-            (println "draw image")
             (let [x-graphics (draw @s/img-graphics
                                    (doto @s/img-graphics
                                      (.background 0)
@@ -63,8 +58,7 @@
                       y (range (- pov-y-offset s/pov-height) s/pov-height)]
                 (.image @s/graphics x-graphics x y))))))
   
-  (q/image @s/graphics 0 0)
-  (println "done"))
+  (q/image @s/graphics 0 0))
 
 (defn display-image-list [])
 (defn display-instructions [])
